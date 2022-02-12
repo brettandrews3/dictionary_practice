c04b99623788f53e13e75c858c0fcccc74e4e931
Brett <brettandrews3@gmail.com>
2022-02-12 11:30:59 -0600

    1st commit. I defined my_dictionary as dictionary and listed some key-value pairs.

---
+++ my_dictionary.py
@@ -0,0 +1,9 @@
+# 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
+
+my_dictionary = {}
+
+my_dictionary['jenny'] = '867-5309'
+my_dictionary['paul'] = '555-1201'
+my_dictionary['kramer'] = '555-FILK'
+my_dictionary['brett'] = '915-3224'
+my_dictionary['jen'] = '515-6160'
